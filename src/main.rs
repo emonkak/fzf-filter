bba7eb0d6d76bb051967e7b544d67fc3c6e5c2c1
Shota Nozaki <emonkak@gmail.com>
2023-04-20 13:04:24 +0900

    feat: add '--field-partitions' and '--field-delimiter' options

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,21 @@ USAGE:
   fzf-filter [OPTIONS] -- <COMMAND> [COMMAND_ARGUMENTS]
 
 OPTIONS:
-  -l, --limit-items <N>  a maximum number of items to print
-  -h, --help             Print help information";
+  -l, --limit-items NUM       a maximum number of items to print
+  -f, --field-index INDEX     a field index to be matched
+                              (default: whole line)
+  -p, --field-partitions NUM  a maximum number of partitions of the field
+  -d, --field-delimiter CHAR  a field delimiter character
+                              (default: \\t)
+  -h, --help                  print help information";
 
 fn main() -> anyhow::Result<ExitCode> {
-    let args = Args::parse(env::args_os());
-    match args {
-        Ok(args) => {
-            if args.help {
-                println!("{}", HELP);
-                Ok(ExitCode::SUCCESS)
-            } else {
-                run(args)
-            }
+    let mode = Mode::parse(env::args_os());
+    match mode {
+        Ok(Mode::Run(args)) => run(args),
+        Ok(Mode::Help) => {
+            println!("{}", HELP);
+            Ok(ExitCode::SUCCESS)
         }
         Err(error) => {
             println!("{}", HELP);
@@ -70,10 +72,12 @@ fn run(args: Args) -> anyhow::Result<ExitCode> {
 
             let mut matched_lines = vec![];
 
-            match args.field {
-                Some(field) => {
+            match args.field_index {
+                Some(index) => {
+                    let delimiter = args.field_delimiter;
+                    let partitions = args.field_partitions;
                     for line in output_content.lines() {
-                        if let Some(content) = line.split('\t').nth(field) {
+                        if let Some(content) = extract_field(line, delimiter, index, partitions) {
                             let score = fzf::get_score(content, &pattern, &slab);
                             if score > 0 {
                                 matched_lines.push(Reverse((score, line)));
@@ -112,33 +116,59 @@ fn run(args: Args) -> anyhow::Result<ExitCode> {
 }
 
 #[derive(Debug)]
-struct Args {
-    command: OsString,
-    command_args: Vec<OsString>,
-    field: Option<usize>,
-    help: bool,
-    limit_items: Option<usize>,
+enum Mode {
+    Run(Args),
+    Help,
 }
 
-impl Args {
+impl Mode {
     fn parse(args: ArgsOs) -> anyhow::Result<Self> {
-        let mut head_parts = vec![];
         let mut iter = args.into_iter();
+        let mut main_args = vec![];
         while let Some(arg) = iter.next() {
             if arg.eq_ignore_ascii_case("--") {
                 break;
             }
-            head_parts.push(arg)
+            main_args.push(arg)
         }
-        let command = iter.next().ok_or(anyhow!("command is not specified"))?;
-        let command_args = iter.collect();
-        let mut pico_args = pico_args::Arguments::from_vec(head_parts);
-        Ok(Self {
-            command,
-            command_args,
-            field: pico_args.opt_value_from_str(["-f", "--field"])?,
-            help: pico_args.contains(["-h", "--help"]),
-            limit_items: pico_args.opt_value_from_str(["-l", "--limit-items"])?,
-        })
+        let mut pico_args = pico_args::Arguments::from_vec(main_args);
+        if pico_args.contains(["-h", "--help"]) {
+            Ok(Self::Help)
+        } else {
+            let command = iter.next().ok_or(anyhow!("command is not specified"))?;
+            let command_args = iter.collect();
+            Ok(Self::Run(Args {
+                command,
+                command_args,
+                field_delimiter: pico_args
+                    .opt_value_from_str(["-d", "--field-delimiter"])?
+                    .unwrap_or('\t'),
+                field_index: pico_args.opt_value_from_str(["-f", "--field-index"])?,
+                field_partitions: pico_args.opt_value_from_str(["-p", "--field-partitions"])?,
+                limit_items: pico_args.opt_value_from_str(["-l", "--limit-items"])?,
+            }))
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Args {
+    command: OsString,
+    command_args: Vec<OsString>,
+    field_delimiter: char,
+    field_index: Option<usize>,
+    field_partitions: Option<usize>,
+    limit_items: Option<usize>,
+}
+
+fn extract_field(
+    s: &str,
+    delimiter: char,
+    index: usize,
+    partitions: Option<usize>,
+) -> Option<&str> {
+    match partitions {
+        Some(n) => s.splitn(n, delimiter).nth(index),
+        None => s.split(delimiter).nth(index),
     }
 }
